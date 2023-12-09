10c1256248dd15afaddc0e3b28551b937c7c3468
luoyu123321 <779231460@qq.com>
2023-12-09 14:16:26 +0800

    修改奖品

--- choujiang.js
+++ choujiang.js
@@ -22,7 +22,8 @@ class Tab {
     that.startend = !that.startend
     console.log(1111,this, that, that.startend)
     if(that.startend){
-    const jiangpin = ['苹果手表', '一元参与奖', '一小套化妆品(价值600元)', '带金手串', '一元参与奖','苹果手表', '一元参与奖', '一小套化妆品(价值600元)', '带金手串', '一元参与奖']
+    // const jiangpin = ['苹果手表', '一元参与奖', '一小套化妆品(价值600元)', '带金手串', '一元参与奖','苹果手表', '一元参与奖', '一小套化妆品(价值600元)', '带金手串', '一元参与奖']
+    const jiangpin = ['测试小狗屎1', '测试小垃圾2', '测试罗老师3', '测试床4', '测试瘦5','测试小狗屎1', '测试小垃圾2', '测试罗老师3', '测试床4', '测试瘦5']
     that.timeout = setInterval(()=>{
         const res = parseInt(Math.random() * 10)
         that.content.innerText = jiangpin[res]
