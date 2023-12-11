41f1eb7eca22b73f6af62c08e5ac3b56b1fcd6f5
luoyu123321 <779231460@qq.com>
2023-12-11 22:45:40 +0800

    取消无限抽奖

--- choujiang.js
+++ choujiang.js
@@ -49,8 +49,8 @@ class Tab {
   initData() {
     localStorage.setItem('addcdk', JSON.stringify(['我爱你', '我喜欢你', '我对象最帅']))
     // localStorage.setItem('jiangpin', '[]')
-    localStorage.removeItem('choujiangTimes')
-    localStorage.removeItem('jiangpin')
+    // localStorage.removeItem('choujiangTimes')
+    // localStorage.removeItem('jiangpin')
   }
 
   initFunc() {
